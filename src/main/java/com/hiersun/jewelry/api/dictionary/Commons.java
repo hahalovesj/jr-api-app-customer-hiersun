59b4b9e8aa57d97766b0c22f9ff2c04adb4511d1
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-13 18:32:24 +0800

    commit

--- src/main/java/com/hiersun/jewelry/api/dictionary/Commons.java
+++ src/main/java/com/hiersun/jewelry/api/dictionary/Commons.java
@@ -4,8 +4,8 @@ public class Commons {
 	// 94
 	public static String  PIC_DOMAIN ="http://192.168.4.94/post/";
 	
-	//92
-//	public static String  PIC_DOMAIN = "http://192.168.4.94:8084/jr-apiupload";
+	//93
+//	public static String  PIC_DOMAIN = "http://192.168.4.93:8081/jr-apiupload";
 	
 	public static String HEAD_IOC = "http://192.168.4.93:8081/jr-apisys/1.jpg";
 
