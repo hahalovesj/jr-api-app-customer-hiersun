2a174010ded9cddda99eb7613c03d48938832fab
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-18 14:42:50 +0800

    获取男女

--- src/main/java/com/hiersun/jewelry/api/util/CommonUtils.java
+++ src/main/java/com/hiersun/jewelry/api/util/CommonUtils.java
@@ -1,5 +1,10 @@
 package com.hiersun.jewelry.api.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hiersun.jewelry.api.dictionary.IcoDictionary;
+
 public class CommonUtils {
 	/**
 	 * 把手机号变成 138****1111
@@ -29,5 +34,28 @@ public class CommonUtils {
 	public static String stripHtml(String htmlStr) {
 		String str = htmlStr.replaceAll("<[.[^<]]*>", "");
 		return str;
+	}
+
+	/**
+	 * 获取男女头像    0是女 1是男
+	 */
+	public static Map<String, String> getIco(int i) {
+		if (i > 1) {
+			return null;
+		}
+		int randomKey = (int) com.hiersun.jewelry.api.util.Trunc.getRandomForRange(1, 10);
+		Map<String, String> map = new HashMap<String, String>();
+		String big, small;
+		// 0 是女
+		if (i == 0) {
+			big = IcoDictionary.BIG_WOMAN_PHOTO.get(randomKey);
+			small = IcoDictionary.SMALL_WOMAN_PHOTO.get(randomKey);
+		} else {
+			big = IcoDictionary.BIG_MAN_PHOTO.get(randomKey);
+			small = IcoDictionary.SMALL_MAN_PHOTO.get(randomKey);
+		}
+		map.put("big", big);
+		map.put("small", small);
+		return map;
 	}
 }
