728069bb7aca6b44bcd82711dd40a3569a1261a2
luyadong <luyadong@hiersun.com>
2016-01-21 18:30:17 +0800

    commit

--- src/main/java/com/hiersun/jewelry/api/service/usercenter/CheckSalesAfterAppService.java
+++ src/main/java/com/hiersun/jewelry/api/service/usercenter/CheckSalesAfterAppService.java
@@ -61,8 +61,13 @@ public class CheckSalesAfterAppService implements BaseService {
 			order.setGoodsPicUrl(jrdsOrderVo.getGoodsPic());
 			order.setOrderID(jrdsOrderVo.getId());
 			order.setOrderNO(jrdsOrderVo.getOrderNo());
-			order.setOrderPrice(jrdsOrderVo.getPayAmount().doubleValue());
-			order.setGoodsBuyPrice(jrdsOrderVo.getGoodsPrice().doubleValue());
+			if(jrdsOrderVo.getPayAmount()!=null){
+				order.setOrderPrice(jrdsOrderVo.getPayAmount().doubleValue());
+			}
+			if(jrdsOrderVo.getGoodsPrice()!=null){
+				order.setGoodsBuyPrice(jrdsOrderVo.getGoodsPrice().doubleValue());
+			}
+			
 			order.setFreightDesc("￥0.00元运费（平台包邮）");
 			review = new Review();
 			review.setReviewExplain(jrSalesVo.getInstruction());
