2009bd2c607808842b1f0388214b799df8b67f69
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-21 10:58:21 +0800

    c

--- src/main/java/com/hiersun/jewelry/api/service/usercenter/DirectBuyOrderInfoAppService.java
+++ src/main/java/com/hiersun/jewelry/api/service/usercenter/DirectBuyOrderInfoAppService.java
@@ -119,8 +119,12 @@ public class DirectBuyOrderInfoAppService implements BaseService {
 			if (jrdsOrderVo.getPayTime() != null) {
 				order.setPayTime(DateUtil.dateToStr(jrdsOrderVo.getPayTime(), "yyyy-MM-dd HH:mm:ss"));
 			}
-
-			order.setCancelTime(map.get(11));
+			if (jrdsOrderVo.getOrderStatus().intValue() == 11) {
+				order.setCancelTime(map.get(11));
+			}
+			if (jrdsOrderVo.getOrderStatus().intValue() == 12) {
+				order.setCancelTime(map.get(12));
+			}
 			order.setDeliveryedTime(map.get(9));
 			order.setConfirmedTime(map.get(10));
 			// 鉴定时间 如果8的状态不为空 去抓的时间 8位鉴定通过 发货，7为鉴定不通过 5为验证未通过 modifyBy Li.Long
