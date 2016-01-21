9fd0c6ad190ac84d44108b6135fb116c41598b5d
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-21 14:16:45 +0800

    我的购买，详情时间问题

--- src/main/java/com/hiersun/jewelry/api/service/usercenter/DirectBuyOrderInfoAppService.java
+++ src/main/java/com/hiersun/jewelry/api/service/usercenter/DirectBuyOrderInfoAppService.java
@@ -116,6 +116,7 @@ public class DirectBuyOrderInfoAppService implements BaseService {
 			if (jrdsOrderVo.getPayType() != null) {
 				order.setPayType(QualificationType.PAY_TYPE_MAP.get(jrdsOrderVo.getPayType().intValue()));
 			}
+			// 时间
 			if (jrdsOrderVo.getPayTime() != null) {
 				order.setPayTime(DateUtil.dateToStr(jrdsOrderVo.getPayTime(), "yyyy-MM-dd HH:mm:ss"));
 			}
@@ -125,8 +126,13 @@ public class DirectBuyOrderInfoAppService implements BaseService {
 			if (jrdsOrderVo.getOrderStatus().intValue() == 12) {
 				order.setCancelTime(map.get(12));
 			}
-			order.setDeliveryedTime(map.get(9));
-			order.setConfirmedTime(map.get(10));
+			if (jrdsOrderVo.getOrderStatus().intValue() == 9) {
+				order.setDeliveryedTime(map.get(9));
+			}
+			if (jrdsOrderVo.getOrderStatus().intValue() == 10) {
+				order.setConfirmedTime(map.get(10));
+			}
+
 			// 鉴定时间 如果8的状态不为空 去抓的时间 8位鉴定通过 发货，7为鉴定不通过 5为验证未通过 modifyBy Li.Long
 			order.setAppraisaledTime(map.get(8) != null ? map.get(8) : (map.get(7) != null ? map.get(7)
 					: (map.get(5) != null ? map.get(5) : null)));
