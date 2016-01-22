779423a0b3de30f39c494b5d1c72c4209bd95062
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-22 13:51:10 +0800

    commit

--- src/main/java/com/hiersun/jewelry/api/service/usercenter/DirectBuyOrderInfoAppService.java
+++ src/main/java/com/hiersun/jewelry/api/service/usercenter/DirectBuyOrderInfoAppService.java
@@ -129,6 +129,8 @@ public class DirectBuyOrderInfoAppService implements BaseService {
 
 			order.setDeliveryedTime(map.get(9));
 			order.setConfirmedTime(map.get(10));
+			
+			order.setConfirmedBuyTime(map.get(5));
 
 			// 鉴定时间 如果8的状态不为空 去抓的时间 8位鉴定通过 发货，7为鉴定不通过 5为验证未通过 modifyBy Li.Long
 			order.setAppraisaledTime(map.get(8) != null ? map.get(8) : (map.get(7) != null ? map.get(7)
