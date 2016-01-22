ff0f9a16e7f4bbd2ffadaba28b2f0cdf3760f223
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-22 10:54:37 +0800

    commit

--- src/main/java/com/hiersun/jewelry/api/entity/request/Request4017.java
+++ src/main/java/com/hiersun/jewelry/api/entity/request/Request4017.java
@@ -7,24 +7,17 @@ import com.hiersun.jewelry.api.entity.Body;
 public class Request4017 extends Body {
 	private String orderNO;
 
-
-
 	public String getOrderNO() {
 		return orderNO;
 	}
-
-
-
 
 	public void setOrderNO(String orderNO) {
 		this.orderNO = orderNO;
 	}
 
-
-
 	@Override
 	public int volidateValue() {
-		if(StringUtils.isEmpty(this.getOrderNO())){
+		if (StringUtils.isEmpty(this.getOrderNO())) {
 			return 400710;
 		}
 		return 0;
