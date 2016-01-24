b38023c32bc68245d97c05dd76981b1916b783d2
xueyuan_zhao <xueyuan_zhao@hiersun.com>
2016-01-24 11:53:01 +0800

    搜索头像问题

--- src/main/java/com/hiersun/jewelry/api/dictionary/Commons.java
+++ src/main/java/com/hiersun/jewelry/api/dictionary/Commons.java
@@ -2,11 +2,11 @@ package com.hiersun.jewelry.api.dictionary;
 
 public class Commons {
 	// 94
-	public static String PIC_DOMAIN = "http://192.168.4.94/post/";
-//	public static String PIC_DOMAIN = "http://post.oohdear.net/";
+//	public static String PIC_DOMAIN = "http://192.168.4.94/post/";
+	public static String PIC_DOMAIN = "http://post.oohdear.net/";
 	// 93
 //	public static String PIC_DOMAIN = "http://192.168.4.93:8081/jr-apiupload";
 
-	public static String HEAD_IOC = "http://192.168.4.93:8081/jr-apisys/1.jpg";
+//	public static String HEAD_IOC = "http://192.168.4.93:8081/jr-apisys/1.jpg";
 
 }
